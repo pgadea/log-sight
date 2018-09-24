0f94316be061e762d31383d69c89ddad96cfefb9
Pierre <pgadea7@gmail.com>
2018-09-23 22:22:41 -0500

    Created server component

--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -1,15 +1,75 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Order } from '../../shared/order';
 @Component({
   selector: 'app-section-orders',
   templateUrl: './section-orders.component.html',
   styleUrls: ['./section-orders.component.css']
 })
 export class SectionOrdersComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  orders: Order[] = [
+    {
+      id: 1,
+      customer: {
+        id: 1,
+        name: 'Main St Bakery',
+        state: 'CO',
+        email: 'mainst@example.com'
+      },
+      total: 230,
+      placed: new Date(2018, 12, 1),
+      fulfilled: new Date(2018, 12, 2)
+    },
+    {
+      id: 2,
+      customer: {
+        id: 1,
+        name: 'Main St Bakery',
+        state: 'CO',
+        email: 'mainst@example.com'
+      },
+      total: 230,
+      placed: new Date(2018, 12, 1),
+      fulfilled: new Date(2018, 12, 2)
+    },
+    {
+      id: 3,
+      customer: {
+        id: 1,
+        name: 'Main St Bakery',
+        state: 'CO',
+        email: 'mainst@example.com'
+      },
+      total: 230,
+      placed: new Date(2018, 12, 1),
+      fulfilled: new Date(2018, 12, 2)
+    },
+    {
+      id: 4,
+      customer: {
+        id: 1,
+        name: 'Main St Bakery',
+        state: 'CO',
+        email: 'mainst@example.com'
+      },
+      total: 230,
+      placed: new Date(2018, 12, 1),
+      fulfilled: new Date(2018, 12, 2)
+    },
+    {
+      id: 5,
+      customer: {
+        id: 1,
+        name: 'Main St Bakery',
+        state: 'CO',
+        email: 'mainst@example.com'
+      },
+      total: 230,
+      placed: new Date(2018, 12, 1),
+      fulfilled: new Date(2018, 12, 2)
+    }
+  ];
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
