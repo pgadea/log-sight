fc244e4549e137f8a2371c37f74fd9193ca37038
Pierre <pgadea7@gmail.com>
2018-09-30 13:56:07 -0500

    Pagination complete and fixed bugs on pagination

--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -1,75 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { Order } from '../../shared/order';
+import { SalesDataService } from '../../services/sales-data.service';
 @Component({
   selector: 'app-section-orders',
   templateUrl: './section-orders.component.html',
   styleUrls: ['./section-orders.component.css']
 })
 export class SectionOrdersComponent implements OnInit {
-  constructor() {}
+  constructor(private _salesData: SalesDataService) {}
 
-  orders: Order[] = [
-    {
-      id: 1,
-      customer: {
-        id: 1,
-        name: 'Main St Bakery',
-        state: 'CO',
-        email: 'mainst@example.com'
-      },
-      total: 230,
-      placed: new Date(2018, 12, 1),
-      fulfilled: new Date(2018, 12, 2)
-    },
-    {
-      id: 2,
-      customer: {
-        id: 1,
-        name: 'Main St Bakery',
-        state: 'CO',
-        email: 'mainst@example.com'
-      },
-      total: 230,
-      placed: new Date(2018, 12, 1),
-      fulfilled: new Date(2018, 12, 2)
-    },
-    {
-      id: 3,
-      customer: {
-        id: 1,
-        name: 'Main St Bakery',
-        state: 'CO',
-        email: 'mainst@example.com'
-      },
-      total: 230,
-      placed: new Date(2018, 12, 1),
-      fulfilled: new Date(2018, 12, 2)
-    },
-    {
-      id: 4,
-      customer: {
-        id: 1,
-        name: 'Main St Bakery',
-        state: 'CO',
-        email: 'mainst@example.com'
-      },
-      total: 230,
-      placed: new Date(2018, 12, 1),
-      fulfilled: new Date(2018, 12, 2)
-    },
-    {
-      id: 5,
-      customer: {
-        id: 1,
-        name: 'Main St Bakery',
-        state: 'CO',
-        email: 'mainst@example.com'
-      },
-      total: 230,
-      placed: new Date(2018, 12, 1),
-      fulfilled: new Date(2018, 12, 2)
-    }
-  ];
+  orders: Order[];
+  total = 0;
+  page = 1;
+  limit = 10;
+  loading = false;
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getOrders();
+  }
+  getOrders(): void {
+    this._salesData.getOrders(this.page, this.limit)
+      .subscribe(res => {
+        this.orders = res['page']['data'];
+        this.total = res['page'].total;
+        this.loading = false;
+      });
+  }
+
+  goToPrevious(): void {
+    this.page--;
+    this.getOrders();
+  }
+
+  goToNext(): void {
+    this.page++;
+    this.getOrders();
+  }
+
+  goToPage(n: number): void {
+    this.page = n;
+    this.getOrders();
+  }
 }
--- src/app/services/server.service.ts
+++ src/app/services/server.service.ts
@@ -1,8 +1,44 @@
 import { Injectable } from '@angular/core';
+import { Http, RequestOptions, Response, Headers } from '@angular/http';
+import { Observable, throwError } from 'rxjs';
+import { ServerMessage } from '../shared/server-message';
+import { Server } from '../shared/server';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable()
 export class ServerService {
 
-  constructor() { }
+  constructor(private _http: Http) {
+    this.headers = new Headers({
+      'Content-Type' : 'application/json',
+      'Accept' : 'q=0.8;application/json;q=0.9'
+    });
 
+    this.options = new RequestOptions({ headers: this.headers });
+  }
+
+  options: RequestOptions;
+  headers: Headers;
+
+  getServers(): Observable<Server[]> {
+    return this._http.get('http://localhost:5000/api/server').pipe(
+      map(res => res.json()),
+      catchError(this.handleError)
+    );
+  }
+
+  handleError(error: any) {
+    const errMsg = error.message
+      ? error.message
+      : error.status
+        ? `${error.status} - ${error.statusText}`
+        : 'Server error';
+
+    return throwError(errMsg);
+  }
+
+  handleServerMessage(msg: ServerMessage): Observable<Response> {
+    const url = 'http://localhost:5000/api/server/' + msg.id;
+    return this._http.put(url, msg, this.options).pipe(map(res => res.json()));
+  }
 }
